32dec4db4634859f0222714b674fc3a42fe23571
= <=>
2026-09-09 13:05:56 +0700

    babak 1 pilihan 1

--- script.py
+++ script.py
@@ -1,1 +1,20 @@
-print("test")
+nyawa = 100
+baterai_senter = True
+punya_kunci = False
+punya_peta = False
+
+print('=== PULAU MISTERIUS: TINGKAT LANJUT ===')
+print('Kamu terbangun di tepi pantai yang berkabut tebal.')
+print('Di tanganmu hanya ada sebuah senter (baterai tersisa sedikit) dan kompas')
+
+print('[BABAK 1: PERSIMPANGAN PANTAI]')
+print('1. Masuk ke dalam hutan lebat yang geap gulita')
+print('2. Menyusuri tebing karang curam yang dihantam ombak.')
+pilihan_babak_1 = input('masukkan pilihanmu (1/2): ')
+
+if pilihan_babak_1 == '1':
+    print('Kamu melangkah masuk di dalam hutan. Suasana sangat mencekam. Senter-mu tiba-tiba berkedip dan hampir mati karena baterai melemah')
+
+
+
+
